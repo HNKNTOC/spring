1e1399fcba13a3db41b245baad165592c232724b
HNKNTOC <nik_luzin@list.ru>
2024-02-26 11:34:04 +0400

    first commit

---
+++ src/main/java/com/example/SpringDZ/Model/RequestDTO.java
@@ -0,0 +1,58 @@
+package com.example.SpringDZ.Model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class RequestDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String openDate;
+    private String closeDate;
+
+    public String getRqUID() {
+        return rqUID;
+    }
+
+    public void setRqUID(String rqUID) {
+        this.rqUID = rqUID;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getOpenDate() {
+        return openDate;
+    }
+
+    public void setOpenDate(String openDate) {
+        this.openDate = openDate;
+    }
+
+    public String getCloseDate() {
+        return closeDate;
+    }
+
+    public void setCloseDate(String closeDate) {
+        this.closeDate = closeDate;
+    }
+}
---
+++ src/main/java/com/example/SpringDZ/Model/ResponseDTO.java
@@ -0,0 +1,69 @@
+package com.example.SpringDZ.Model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ResponseDTO {
+
+    private String rqUID;
+    private String clientId;
+    private String account;
+    private String currency;
+    private BigDecimal balance;
+    private BigDecimal maxLimit;
+
+    public String getRqUID() {
+        return rqUID;
+    }
+
+    public void setRqUID(String rqUID) {
+        this.rqUID = rqUID;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public BigDecimal getMaxLimit() {
+        return maxLimit;
+    }
+
+    public void setMaxLimit(BigDecimal maxLimit) {
+        this.maxLimit = maxLimit;
+    }
+}
